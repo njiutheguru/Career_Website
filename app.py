af493404312b547a552770b1eebeae89d30c648d
Njiu <134121008+njiutheguru@users.noreply.github.com>
2024-06-06 22:45:19 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return 'hello world, this is steve a flask developer. If i may ask, what is this bigger hype about django whereas flask is the real thing?'
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
-
+    
 
